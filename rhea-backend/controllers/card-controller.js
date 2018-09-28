5829c38627308242e1aee59b0a200791f9250eb4
William Yang <williamyang93@gmail.com>
2018-09-28 11:36:36 -0700

    Updated card to cardset for better naming convention Fixed delete end point

--- rhea-backend/controllers/card-controller.js
+++ rhea-backend/controllers/card-controller.js
@@ -3,11 +3,12 @@ const FlashCardData = require('../data/flashcards');
 
 const help = (req, res) => {
     res.status(200).send({ message: "Welcome to Flashcard API", 
-                           GetAllCards: "/all",
-                           GetCardById: "/:id",
-                           CreateCard: "/",
-                           EditCard: "/:id",
-                           DeleteCard: '/:id',
+                           Seed:           "/seed",
+                           GetAllCardsets: "/all",
+                           GetCardSetById: "/:id",
+                           CreateCardSet:  "/",
+                           EditCardSet:    "/:id",
+                           DeleteCardSet:  "/:id",
                         })
 };
 
@@ -20,64 +21,65 @@ const seed = async (req, res) => {
     }
 }
 
-const getAllCards = async (req, res) => {
+const getAllCardSets = async (req, res) => {
     try {
         let allCards = await FlashCards.find().exec()
         if(allCards.length === 0) res.status(404).send({ success: false, message: `No flashcards found in database`})
-        else res.status(200).send({ success:true, cards: allCards })
+        else res.status(200).send({ success:true, cardsets: allCards })
     } catch (e) {
         res.status(500).send({success: false, message: `Server Error: ${e.message}`})
     }
 }
 
-const getCardById = async (req, res) => {
+const getCardSetById = async (req, res) => {
     let { id } = req.params
     try {
-        let card = await FlashCards.findById(id).exec();
-        if (!ride) res.status(404).send({ success: false, message: `No Card Found by ID: ${id}`})
-        else res.status(200).send({ success: true, card})
+        let cardSet = await FlashCards.findById(id).exec();
+        if (!cardSet) res.status(404).send({ success: false, message: `No Card Found by ID: ${id}`})
+        else res.status(200).send({ success: true, cardSet})
     } catch(e){
         if(e.name === 'CastError') res.status(406).send({success: false, message: `Cast Error: ${e.message}`})
         else res.status(500).send({success: false, message: `Server Error: ${e.message}`})
     }
 }
 
-const createCard = (req, res) => {
-    let cards = req.body;
-    console.log(cards)
-    FlashCards.create(cards)
-        .then((cards) => {
-            res.status(200).send(cards);
+const createCardSet = (req, res) => {
+    let cardSet = req.body;
+    console.log(cardSet)
+    FlashCards.create(cardSet)
+        .then((cardSet) => {
+            res.status(200).send(cardSet);
         })
         .catch((err) => {
             res.status(500).send({ Error: err.message })
         })
 }
 
-const editCard = (req, res) => {
-    FlashCards.findByIdAndUpdate(req.params.id, { $set: req.body }, (err, cards) => {
+const editCardSet = (req, res) => {
+    FlashCards.findByIdAndUpdate(req.params.id, { $set: req.body }, {new:true}, (err, cardSet) => {
         if(err) {
             res.status(500).send({ 
                 message:'There was an error with out database.', error: err.message
             })
-        } else if (!cards) {
+        } else if (!cardSet) {
             res.status(404).send({ message: 'Could not find cards with that ID' })
         } else {
-            res.status(200).send(cards)
+            res.status(200).send(cardSet)
         }
     })
 }
 
-const deleteCard = (req, res) => {
-    FlashCards.findByIdAndRemove(req.params.id, (err, cards) => {
+const deleteCardSet = (req, res) => {
+    let {id} = req.params.id
+    FlashCards.findByIdAndRemove(id, (err, cardSet) => {
         if(err) {
             res.status(500).send({ message: 'There was an error with our database.', error: err.message })
-        } else if (!cards) {
+        } else if (!cardSet) {
             res.status(404).send({ message: 'Could not find cardset with that id' });
         } else {
-            res.status(200). send({ deletedCards: cards })
+            res.status(200). send({ message: `Cardset ${id} successfully deleted.`, cardset: cardSet })
         }
     })
 }
 
-module.exports = { help, seed, getAllCards, getCardById, createCard, editCard, deleteCard };
+module.exports = { help, seed, getAllCardSets, getCardSetById, createCardSet, editCardSet, deleteCardSet };
